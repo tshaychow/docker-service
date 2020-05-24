a33dfd5e12a720422838f974304e0d997396fe93
Eric Chow <TsEric@tshay.fritz.box>
2020-05-24 22:37:37 +0200

    inital commit

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import numpy as np
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+class Content(Resource):
+    def get(self): 
+        return{
+            'output':json.dumps((np.random.normal(0,1,100)).tolist())
+        }
+
+
+api.add_resource(Content,'/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port = 80, debug=True)
